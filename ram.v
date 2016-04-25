d779b46e2e323817ca7c010eb14b4ccae4314a9c
samlnx03 <samlnx03@hotmail.com>
2016-04-25 17:17:53 -0500

    Update ram.v

--- ram.v
+++ ram.v
@@ -7,3 +7,23 @@ module ram(input clk, enable, [3:0]ab, output [3:0]dbi, output [3:0] dbo);
         end
 endmodule
 
+module Monostable(Dclk, In, Out);
+parameter Ns = 100;
+input wire Dclk;   // Delay clock (10ns)
+input wire In;     // Data to be delayed 
+output reg Out;    // Output
+
+integer Count;
+                                
+always @(posedge Dclk)
+begin
+    if (In) begin
+        Out <= 1;
+        Count <= 0;
+    end else begin
+        Count <= Count + 1;
+        if (Count == Ns/10) Out <= 0;
+    end
+end
+endmodule
+
